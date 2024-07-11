5ba3acd897bf11b710369dceca2d1831af244471
Vasilii Tikhonov <tihonovvasilij154@gmail.com>
2024-07-11 17:32:22 +0300

    added Table View and some logic

--- Diary of plans/Model/Note.swift
+++ Diary of plans/Model/Note.swift
@@ -19,3 +19,299 @@ struct Note: Codable, Identifiable, Equatable {
         lhs.id == rhs.id
     }
 }
+
+
+/*
+ Ты IOS разработчик.У нас есть часть кода для приложение Заметок. Нужно дописать код, отобразить таблицу на экране, сделать так чтобы в верхнем правом углу была кнопка для добавления заметки, и чтобы заметку можно было удалять из таблицы и помечать как завершенную (isComplete).
+ 
+ You are an iOS developer.We have a piece of code for the Notes app. You need to add the code, display the table on the screen, make sure that there is a button in the upper right corner to add a note, and that the note can be deleted from the table and marked as completed (isComplete).
+ 
+ Here is the code:
+ 
+ import Foundation
+ import UIKit
+ 
+ protocol NoteViewProtocol: AnyObject {
+     func showError(title: String, message: String)
+     func showLoading()
+     func hideLoading()
+     func loadRow(at index: Int)
+     func reloadData()
+     func didInsertRow(at index: Int)
+     func didDeleteRow(at index: Int)
+ }
+
+
+
+ class NoteViewController: UIViewController {
+
+     override func viewDidLoad() {
+         super.viewDidLoad()
+     }
+
+
+ }
+ 
+ 
+ 
+
+ struct Note: Codable, Identifiable, Equatable {
+     
+     var id = UUID()
+     var title: String
+     var isComplete: Bool
+     var date: Date
+     var notes: String
+ 
+     static func == (lhs: Self, rhs: Self) -> Bool {
+         lhs.id == rhs.id
+     }
+ }
+ protocol FileHandlerProtocol {
+     typealias FetchCompletion = (Result<Data, Error>) -> Void
+     typealias WriteCompletion = (Result<Void, Error>) -> Void
+     typealias Encoderesult = Result<Data, Error>
+     
+     func fetch(completion: @escaping FetchCompletion)
+     func write(_ data: Data, completion: @escaping WriteCompletion)
+     func encodeNotes(_ notes: [Note]) -> Encoderesult
+ }
+
+ class FileHandler: FileHandlerProtocol {
+
+     private var url: URL
+     
+     init() {
+         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+         url = documentsDirectory.appending(component: "todo").appendingPathExtension("plist")
+         
+         if !FileManager.default.fileExists(atPath: url.path) {
+             FileManager.default.createFile(atPath: url.path, contents: nil)
+         }
+     }
+     
+     func fetch(completion: @escaping FetchCompletion) {
+         do {
+            let data = try Data(contentsOf: url)
+             completion(.success(data))
+         } catch {
+             completion(.failure(error))
+         }
+     }
+     
+     func write(_ data: Data, completion: @escaping WriteCompletion) {
+         do {
+             try data.write(to: url)
+             completion(.success(()))
+         } catch {
+             completion(.failure(error))
+         }
+     }
+     
+     func encodeNotes(_ notes: [Note]) -> Encoderesult {
+         
+         let propertyListEncoder = PropertyListEncoder()
+         if let encodedList = try? propertyListEncoder.encode(notes) {
+             return .success(encodedList)
+         } else {
+             return.failure(NoteServiceError.fileWriteError("Не удалось закодировать массив заметок в тип Data"))
+         }
+     }
+ }
+ protocol ServiceRepositoryProtocol {
+     typealias FetchNotesCompletion = (Result<[Note], Error>) -> Void
+     typealias OperationCompletion  = (Result<Void, Error>) -> Void
+     
+     func fetchNotes(completion: @escaping FetchNotesCompletion )
+     func saveData(note: Note, completion: @escaping OperationCompletion )
+     func removeData(note: Note, completion: @escaping OperationCompletion )
+     func updateNote(_ note: Note, completion: @escaping OperationCompletion )
+ }
+
+ enum NoteServiceError: Error {
+     case fileReadError(String)
+     case fileWriteError(String)
+     case noteNotFound(String)
+ }
+
+ final class ServiceRepository: ServiceRepositoryProtocol {
+     
+     private let fileHandler: FileHandler
+     
+     private var notesCach = [Note]()
+     
+     init(fileHandler: FileHandler) {
+         self.fileHandler = fileHandler
+     }
+     
+     func fetchNotes(completion: @escaping FetchNotesCompletion) {
+         !notesCach.isEmpty ? completion(.success(notesCach)) : nil
+         
+         fileHandler.fetch { result in
+             switch result {
+             case .success(let dataContent):
+                 guard !dataContent.isEmpty else {
+                     print("Файл пуст")
+                     completion(.success([]))
+                     return
+                 }
+                 let propertyListDecoder = PropertyListDecoder()
+                 guard let notes = try? propertyListDecoder.decode([Note].self, from: dataContent) else {
+                     completion(.failure(NoteServiceError.fileReadError("Не удалось декодировать данные в массив заметок")))
+                     return
+                 }
+                 self.notesCach = notes
+                 completion(.success(notes))
+                 
+             case .failure(let error):
+                 completion(.failure(error))
+             }
+         }
+     }
+     
+     func saveData(note: Note, completion: @escaping OperationCompletion) {
+         notesCach.append(note)
+         switch fileHandler.encodeNotes(notesCach) {
+             
+         case .success(let data):
+             fileHandler.write(data, completion: completion)
+         case .failure(let error):
+             completion(.failure(error))
+         }
+         
+     }
+     
+     func removeData(note: Note, completion: @escaping OperationCompletion) {
+         guard let index = notesCach.firstIndex(where: { noteElement in
+             noteElement == note
+         }) else {
+             completion(.failure(NoteServiceError.noteNotFound("Не удалось найти заметку")))
+             return
+         }
+         notesCach.remove(at: index)
+         
+         switch fileHandler.encodeNotes(notesCach) {
+             
+         case .success(let data):
+             fileHandler.write(data, completion: completion)
+         case .failure(let error):
+             completion(.failure(error))
+         }
+     }
+     
+     func updateNote(_ noteToUpdate: Note, completion: @escaping OperationCompletion) {
+      
+         guard let index = notesCach.firstIndex(where: { noteElement in
+             noteElement == noteToUpdate
+         }) else {
+             completion(.failure(NoteServiceError.noteNotFound("Не удалось найти заметку")))
+             return
+         }
+         notesCach[index] = noteToUpdate
+         
+         switch fileHandler.encodeNotes(notesCach) {
+             
+         case .success(let data):
+             fileHandler.write(data, completion: completion)
+         case .failure(let error):
+             completion(.failure(error))
+         }
+     }
+ }
+ protocol NotePresenterProtocol {
+     
+     func loadAndUpdateDisplayData()
+     func addNote(note: Note)
+     func deleteNote(at index: Int)
+     func numberOfNotes() -> Int
+     func noteAt(index: Int) -> Note
+     func getImage(for isComplete: Bool) -> String
+     func isToggleNote(for index: Int)
+ }
+
+ class NotePresenter: NotePresenterProtocol {
+     
+     private weak var view: NoteViewProtocol?
+     private var dataRepository: ServiceRepositoryProtocol
+     private var notes: [Note] = []
+     
+     init(view: NoteViewProtocol, dataRepository: ServiceRepositoryProtocol) {
+         self.view = view
+         self.dataRepository = dataRepository
+     }
+     
+     func loadAndUpdateDisplayData() {
+         view?.showLoading()
+         dataRepository.fetchNotes { [weak self] result in
+             switch result {
+             case .success(let notesData):
+                 self?.notes = notesData.sorted(by: {$0.date > $1.date })
+                 self?.view?.reloadData()
+                 self?.view?.hideLoading()
+             case .failure(let error):
+                 self?.view?.showError(title: "Ошибка", message: error.localizedDescription)
+                 self?.view?.hideLoading()
+             }
+         }
+     }
+     
+     func addNote(note: Note) {
+         view?.showLoading()
+         dataRepository.saveData(note: note) { [weak self] result in
+             switch result {
+             case .success():
+                 self?.notes = self?.notes.sorted(by: {$0.date > $1.date }) ?? []
+                 self?.view?.didInsertRow(at: 0)
+                 self?.view?.hideLoading()
+             case .failure(let error):
+                 self?.view?.hideLoading()
+                 self?.view?.showError(title: "Ошибка", message: error.localizedDescription)
+             }
+         }
+     }
+     
+     func deleteNote(at index: Int) {
+         let note = notes[index]
+         dataRepository.removeData(note: note) { [weak self] result in
+             switch result {
+             case .success():
+                 self?.notes.remove(at: index)
+                 self?.view?.didDeleteRow(at: index)
+                 
+             case .failure(let error):
+                 self?.view?.showError(title: "Ошибка", message: error.localizedDescription)
+             }
+         }
+     }
+     
+     func numberOfNotes() -> Int {
+         return notes.count
+     }
+     
+     func noteAt(index: Int) -> Note {
+         return notes[index]
+     }
+     
+     func getImage(for isComplete: Bool) -> String {
+         return isComplete ? "checkmark.circle.fill" : "circle"
+     }
+     
+     func isToggleNote(for index: Int) {
+         var note = notes[index]
+         note.isComplete.toggle()
+         
+         dataRepository.updateNote(note) { [weak self] result in
+             switch result {
+                 
+             case .success():
+                 self?.notes[index] = note
+                 self?.view?.loadRow(at: index)
+             case .failure(let error):
+                 self?.view?.showError(title: "Ошибка", message: error.localizedDescription)
+             }
+             
+         }
+     }
+ }
+ 
+ */
--- Diary of plans/View/NoteViewController.swift
+++ Diary of plans/View/NoteViewController.swift
@@ -17,15 +17,118 @@ protocol NoteViewProtocol: AnyObject {
     func didDeleteRow(at index: Int)
 }
 
+class NoteViewController: UIViewController, NoteViewProtocol {
 
-
-class NoteViewController: UIViewController {
-
+    private var tableView: UITableView!
+    private var presenter: NotePresenterProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setupPresenter()
+        presenter.loadAndUpdateDisplayData()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        title = "Notes"
+        
+        // Setup TableView
+        tableView = UITableView(frame: view.bounds)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "NoteCell")
+        view.addSubview(tableView)
+        
+        // Setup Add Button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNoteTapped))
+    }
+    
+    private func setupPresenter() {
+        let fileHandler = FileHandler()
+        let serviceRepository = ServiceRepository(fileHandler: fileHandler)
+        presenter = NotePresenter(view: self, dataRepository: serviceRepository)
+    }
+    
+    @objc private func addNoteTapped() {
+        let alertController = UIAlertController(title: "New Note", message: "Enter note title", preferredStyle: .alert)
+        alertController.addTextField { textField in
+            textField.placeholder = "Note title"
+        }
+        
+        let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
+            guard let textField = alertController.textFields?.first, let noteTitle = textField.text, !noteTitle.isEmpty else {
+                self?.showError(title: "Error", message: "Note title cannot be empty")
+                return
+            }
+            let newNote = Note(title: noteTitle, isComplete: false, date: Date(), notes: "")
+            self?.presenter.addNote(note: newNote)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(addAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 
-
+    // MARK: - NoteViewProtocol Methods
+    func showError(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showLoading() {
+        // Optionally implement loading indicator
+    }
+    
+    func hideLoading() {
+        // Optionally hide loading indicator
+    }
+    
+    func loadRow(at index: Int) {
+        tableView.reloadRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+    }
+    
+    func reloadData() {
+        tableView.reloadData()
+    }
+    
+    func didInsertRow(at index: Int) {
+        tableView.insertRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+    }
+    
+    func didDeleteRow(at index: Int) {
+        tableView.deleteRows(at: [IndexPath(row: index, section: 0)], with: .automatic)
+    }
 }
 
+// MARK: - UITableViewDataSource Methods
+extension NoteViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.numberOfNotes()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath)
+        let note = presenter.noteAt(index: indexPath.row)
+        cell.textLabel?.text = note.title
+        cell.accessoryType = note.isComplete ? .checkmark : .none
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate Methods
+extension NoteViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter.isToggleNote(for: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            presenter.deleteNote(at: indexPath.row)
+        }
+    }
+}
